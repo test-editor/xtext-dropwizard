7cc1ae101a3563e7d498a0a3062ca30c9c210572
Gunther Bachmann <gunther.bachmann@itemis.de>
2018-10-17 11:43:41 +0200

    fix test (and some setup)

--- shell.nix
+++ shell.nix
@@ -10,8 +10,9 @@ stdenv.mkDerivation {
         # do some gradle "finetuning"
         alias g="./gradlew"
         alias g.="../gradlew"
-        export GRADLE_OPTS="$GRADLE_OPTS -Dorg.gradle.daemon=false -Dfile.encoding=utf-8"
+        export GRADLE_OPTS="$GRADLE_OPTS -Dorg.gradle.daemon=false -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8"
         # in case of any local java installations
+        export JAVA_TOOL_OPTIONS="$_JAVA_OPTIONS  -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8"
         export JAVA_HOME=$(readlink $(dirname $(which java)))/..
     '';
 }
